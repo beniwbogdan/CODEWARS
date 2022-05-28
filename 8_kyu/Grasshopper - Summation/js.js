b656d4ab3905c9e329162f969d39a588800fb337
beniv_bohdan <bohdanbeniv@gmail.com>
2022-05-28 09:35:46 +0200

    task 12 done optimization

--- 8_kyu/Grasshopper - Summation/js.js
+++ 8_kyu/Grasshopper - Summation/js.js
@@ -1,14 +1,13 @@
 var summation = function (num) {
     // Code here
-    let sum1 = 0;
     let sum = 0;
 
+
     for (let i = 1; i <= num; i++) {
-        sum1 = i;
-        sum = sum + sum1;
+        sum += i;
 
     }
     console.log(sum);
 }
 
-summation(5);
+summation(52);
